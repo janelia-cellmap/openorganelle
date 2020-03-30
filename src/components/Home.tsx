caa5b31a883ae8913c5af44ab94d2d0fbf1d6d2a
Jody Clements <jody@neomorphic.co.uk>
2020-03-30 11:53:35 -0400

    Adds pagination for the dataset display.
    This fixes #3

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Pagination from '@material-ui/lab/Pagination';
 import Markdown from "react-markdown/with-html"
 import thumbnail from "./cosem3d.png";
 import {makeDatasets} from "../api/datasets";
@@ -10,14 +11,14 @@ import { Grid, Divider } from "@material-ui/core";
 
 const neuroglancerAddress = "http://neuroglancer-demo.appspot.com/#!";
 
-const useStyles = makeStyles(theme => ({  
+const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
     margin: theme.spacing(2)
   },
-  grid: {     
+  grid: {
    direction: "row",
    alignItems: "center",
    justify:"center"
@@ -48,22 +49,35 @@ export default function Home() {
   const classes = useStyles();
   const [datasets, setDatasets] = useState([]);
   const [mdText, setMdText] = useState('');
-  
-  useEffect(() => {    
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const datasetsPerPage = 10;
+
+  useEffect(() => {
     const datasets = makeDatasets('janelia-cosem-dev');
-    datasets.then(setDatasets);
-    // Temporary for testing markdown rendering 
+    datasets.then(created => {
+      setDatasets(created);
+    });
+    // Temporary for testing markdown rendering
     const mdText = fetch('https://raw.githubusercontent.com/janelia-cosem/dataset_descriptions/master/HeLa_Cell2_4x4x4nm/data-portal-description.md', {cache: "reload"}).then((response) => response.text());
     mdText.then(setMdText);
-                  }, []);
+  }, []);
 
   // this loop will be where you modify the meta information and generate
   // the urls.
-  const displayedDataSets = datasets.map(dataset => {
+
+  const rangeStart = (currentPage - 1) * datasetsPerPage;
+  const rangeEnd = rangeStart + datasetsPerPage;
+  const totalPages = Math.ceil(datasets.length / datasetsPerPage);
+
+  const displayedDataSets = datasets.slice(rangeStart, rangeEnd).map((dataset, i) => {
+    const key=`${dataset.path}_${rangeStart}_${i}`;
     return (
-      <Paper key={dataset.path} className={classes.paper}>
-        <Grid container className={classes.grid} spacing={10}>           
-          <Grid item xs zeroMinWidth> <Markdown source={mdText} escapeHtml={false} className={classes.markdown}/> </Grid>
+      <Paper key={key} className={classes.paper}>
+        <Grid container className={classes.grid} spacing={10}>
+          <Grid item xs zeroMinWidth>
+            <Markdown source={mdText} escapeHtml={false} className={classes.markdown}/>
+          </Grid>
           <Divider orientation="vertical"/>
           <Grid item> <a href={`${neuroglancerAddress}${dataset.neuroglancerURLFragment}`} target="_blank" rel="noopener noreferrer">View with neuroglancer</a> </Grid>
         </Grid>
@@ -84,7 +98,12 @@ export default function Home() {
         />
       </div>
       <div className="content">
-        <Container maxWidth="md">{displayedDataSets}</Container>
+        <Container maxWidth="md">
+          <p>{rangeStart + 1} to {Math.min(rangeEnd, datasets.length)} of {datasets.length}</p>
+          { datasets.length > datasetsPerPage && <Pagination count={totalPages} page={currentPage} onChange={(e, value) => setCurrentPage(value)} />}
+          {displayedDataSets}
+          { datasets.length > datasetsPerPage && <Pagination count={totalPages} page={currentPage} onChange={(e, value) => setCurrentPage(value)} />}
+        </Container>
       </div>
     </>
   );
