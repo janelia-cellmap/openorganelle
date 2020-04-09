93dfbcdd852d0ae61626e68476324aad640a932f
Jody Clements <jody@neomorphic.co.uk>
2020-04-09 11:34:29 -0400

    Adds janelia logo/link & changes color of Appbar.
    There is now a link to the janelia website on the top right and the
    colors have been changed to a gradient of the hhmi colors.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,34 +4,39 @@ import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import logo from '../logo.png';
+import logo from "../logo.png";
+import janelia from "../janelia_logo.png";
 
 const useStyles = makeStyles(theme => ({
   link: {
     color: theme.palette.primary.contrastText,
-    textDecoration: 'none',
-    marginLeft: '1em'
+    textDecoration: "none",
+    marginLeft: "1em"
   },
   spacer: {
     flexGrow: 1
   },
   logo: {
-    maxHeight: '40px',
+    maxHeight: "40px",
     marginRight: theme.spacing(2)
+  },
+  appbar: {
+    /* Chrome 10-25, Safari 5.1-6 */
+    background: "-webkit-linear-gradient(to right, #048c94,#50a96e,#8ac04a)",
+    background: "linear-gradient(to right, #048c94,#50a96e,#8ac04a)"
   }
 }));
-
 
 export default function Navigation() {
   const classes = useStyles();
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.appbar}>
       <Toolbar variant="dense">
         <Link className={classes.link} to="/">
-          <img src={logo} alt="cosem logo" className={classes.logo}/>
+          <img src={logo} alt="cosem logo" className={classes.logo} />
         </Link>
         <Link className={classes.link} to="/">
           <Typography variant="h6" color="inherit" noWrap>
@@ -49,6 +54,13 @@ export default function Navigation() {
             Settings
           </Typography>
         </Link>
+        <a href="https://janelia.org" className={classes.link}>
+          <img
+            src={janelia}
+            alt="Janelia Research Campus"
+            className={classes.logo}
+          />
+        </a>
       </Toolbar>
     </AppBar>
   );
