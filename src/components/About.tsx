15b89d51d9f2eee5362e6a5401da3d95cb880df3
Davis Vann Bennett <davis.v.bennett@gmail.com>
2020-06-09 12:06:01 -0400

    change text in about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="about">
       <Typography variant="h3" gutterBottom>
-        About these datasets
+      TBD
       </Typography>
     </div>
   );
