c6de95c50b54ed080e3fa5c9b40467a43040cfe8
Jody Clements <jody@neomorphic.co.uk>
2020-08-04 17:25:30 -0400

    Adds Software, Tutorials & Publications pages.
    This adds the navigation links, components, and routes to allow
    navigation to each of the new pages.

---
+++ src/components/Tutorials.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export default function Tutorials() {
+  return (
+    <div>
+      <Typography variant="h3" gutterBottom>
+        Tutorials
+      </Typography>
+    </div>
+  );
+}
