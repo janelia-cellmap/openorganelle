959a60550ca211858f64da93e3f8e11194d8de22
Jody Clements <jody@neomorphic.co.uk>
2020-10-16 11:32:03 -0400

    Adds content to Software, About & Tutorials pages.
    This is the first pass at adding content to these pages and will need
    additional work to fill them out.

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -7,6 +7,121 @@ export default function Software() {
       <Typography variant="h3" gutterBottom>
         Software
       </Typography>
+
+      <ul>
+        <li>Amira-Avizo (ThermoFisher)</li>
+        <li>
+          FIJI (<a href="https://fiji.sc">https://fiji.sc</a>)
+        </li>
+        <li>Neuroglancer</li>
+        <li>
+          BigDataViewer (
+          <a href="https://imagej.net/BigDataViewer">
+            https://imagej.net/BigDataViewer
+          </a>
+          )
+        </li>
+        <li>
+          n5-viewer (
+          <a href="https://github.com/saalfeldlab/n5-viewer">
+            https://github.com/saalfeldlab/n5-viewer
+          </a>
+          )
+        </li>
+
+        <li>
+          Imglib2 (
+          <a href="https://imagej.net/ImgLib2">https://imagej.net/ImgLib2</a>)
+        </li>
+
+        <li>
+          BigWarp (
+          <a href="https://imagej.net/BigWarp">https://imagej.net/BigWarp</a>)
+        </li>
+        <li>
+          Elastix (
+          <a href="https://elastix.lumc.nl/">https://elastix.lumc.nl/</a>)
+        </li>
+
+        <li>
+          Bigcat (
+          <a href="https://github.com/saalfeldlab/bigcat">
+            https://github.com/saalfeldlab/bigcat
+          </a>
+          )
+        </li>
+
+        <li>
+          Knossos (<a href="https://knossos.app">https://knossos.app</a>)
+        </li>
+
+        <li>OriginPro</li>
+        <li>matplotlib</li>
+
+        <li>docker</li>
+        <li>Tensorflow</li>
+        <li>MongoDB/pymongo</li>
+        <li>SimpleITK</li>
+        <li>Numpy</li>
+        <li>Scipy</li>
+        <li>Zarr</li>
+        <li>Dask</li>
+        <li>Apache spark</li>
+        <li>
+          Gurobi (<a href="https://www.gurobi.com">https://www.gurobi.com</a>)
+        </li>
+        <li>
+          Gunpowder (
+          <a href="https://github.com/funkey/gunpowder/">
+            https://github.com/funkey/gunpowder/
+          </a>
+          )
+        </li>
+
+        <li>
+          CNNectome (
+          <a href="https://github.com/saalfeldlab/CNNectome">
+            https://github.com/saalfeldlab/CNNectome
+          </a>
+          )
+        </li>
+        <li>
+          Simpleference (
+          <a href="https://github.com/saalfeldlab/simpleference">
+            https://github.com/saalfeldlab/simpleference
+          </a>
+          )
+        </li>
+
+        <li>
+          Matlab (
+          <a href="https://www.mathworks.com/products/matlab.html">
+            https://www.mathworks.com/products/matlab.html
+          </a>
+          )
+        </li>
+        <li>
+          n5ng (
+          <a href="https://github.com/janelia-cosem/n5ng">
+            https://github.com/janelia-cosem/n5ng
+          </a>
+          ){" "}
+        </li>
+        <li>
+          waterz (
+          <a href="https://github.com/funkey/waterz">
+            https://github.com/funkey/waterz
+          </a>
+          )
+        </li>
+        <li>
+          Skeletonize3D (
+          <a href="https://github.com/fiji/Skeletonize3D">
+            https://github.com/fiji/Skeletonize3D
+          </a>
+          ){" "}
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Tutorials.tsx
+++ src/components/Tutorials.tsx
@@ -5,8 +5,35 @@ export default function Tutorials() {
   return (
     <div>
       <Typography variant="h3" gutterBottom>
-        Tutorials
+        Open Organelle Tutorials
       </Typography>
+    <Typography variant="h4"> How to navigate website</Typography>
+    <Typography variant="h5">
+    Data
+    </Typography>
+    <Typography variant="h5">
+    Software
+    </Typography>
+    <Typography variant="h5">
+    Tutorials
+    </Typography>
+    <Typography variant="h5">
+    Publications
+    </Typography>
+    <Typography variant="h4">How to navigate data (organization)</Typography>
+    General data and metadata structure?
+    <Typography variant="h4">How to select layers for visualization</Typography>
+    <Typography variant="h4">How to find pre-made data views</Typography>
+    Individual datapages and publication page
+    <Typography variant="h4">How to use Neuroglancer</Typography>
+    Piggy-back on already available documentation?
+    <Typography variant="h4">How to open data in FIJI</Typography>
+    John’s n5 plugin. FIJI icon next to url will copy url to use in the plugin?
+    <Typography variant="h4">Downloading data</Typography>
+    point people to python / command line tools for downloading stuff from s3
+    <Typography variant="h4">How to download analysis</Typography>
+    Csv file on datapage?
+
     </div>
   );
 }
