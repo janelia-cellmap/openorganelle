285ba8a28d092487957c0956dc82c34f93c05eef
Jody Clements <jody@neomorphic.co.uk>
2020-10-28 17:17:47 -0400

    Adds contact and analysis lists to ogranelles page

--- src/components/Organelles.tsx
+++ src/components/Organelles.tsx
@@ -22,6 +22,58 @@ const tableData = [
   ["Vesicle membrane", "", ""]
 ];
 
+const contactSites = [
+  ["ER - Ribosome Contact Sites", "er_ribo_contacts"],
+  ["ER - Golgi Contact Sites", "er_golgi_contacts"],
+  ["ER - Mito Contact Sites", "er_mito_contacts"],
+  ["Endosome - ER Contact Sites", "endo_er_contacts"],
+  ["ER - Plasma Membrane Contat Sites", "er_pm_contacts"],
+  ["ER - Vesicle Contact Sites", "er_vesicle_contacts"],
+  ["Golgi - Vesicle Contact Sites", "golgi_vesicle_contacts"],
+  ["Endosome - Golgi Contact Sites", "endo_golgi_contacts"],
+  ["Mito - Plasma Membrane Contact Sites", "mito_pm_contacts"],
+  ["ER - Microtubule Contact Sites", "er_mt_contacts"],
+  ["Endosome - Microtubule Contact Sites", "endo_mt_contacts"],
+  ["Golgi - Microtubule Contact Sites", "golgi_mt_contacts"],
+  ["Mitochondria - Microtubule Contact Sites", "mito_mt_contacts"],
+  ["Microtubule - Nucleus Contact Sites", "mt_nucleus_contacts"],
+  ["Microtubule - Vesicle Contact Sites", "mt_vesicle_contacts"],
+  ["Microtubule - Plasma Membrane Contact Sites", "mt_pm_contacts"]
+];
+
+const analysisList = [
+  [
+    "Mitochondria Skeletons",
+    "mito_skeleton",
+    "Topological thinning to produce skeletons based on Lee et al., 1994"
+  ],
+  [
+    "Mitochondria Skeletons - Longest Shortest Path",
+    "mito_skeleton-lsp",
+    "Longest shortest path within the pruned skeleton analyzed using Flyoyd Warshall algorithm."
+  ],
+  [
+    "ER Medial Surface",
+    "er_medial-surface",
+    "Topological thinning to produce medial surfaces based on Lee et al., 1994"
+  ],
+  [
+    "Reconstructed ER from Medial Surface with Connected Components",
+    "er_reconstructed",
+    "Connected components of a reconstructed ER, created by expanding the ER medial surface by spheres centered at the medial surface with radii equal to the distance transform at each medial surface voxel."
+  ],
+  [
+    "Reconstructed ER from Medial Surface with Curvature",
+    "er_curvature",
+    "Curvature (planarity) mapped onto er_reconstructed, rescaled from 1 (planarity measure 0) to 255 (planarity measure 1); 0 is background"
+  ],
+  [
+    "Ribosomes classified by contact surface",
+    "ribo_classified",
+    "Ribosomes classified according to the surface they are in contact with. Namely, planar ER, tubular ER, nucleus, and cytosolic (i.e. no contact)."
+  ]
+];
+
 export default function Organelles() {
   const tableRows = tableData.map(row => {
     return (
@@ -35,6 +87,24 @@ export default function Organelles() {
     );
   });
 
+  const contactRows = contactSites.map(row => {
+    return (
+      <TableRow key={row[0]}>
+        <TableCell>{row[0]}</TableCell>
+        <TableCell>{row[1]}</TableCell>
+      </TableRow>
+    );
+  });
+
+  const analysisRows = analysisList.map(row => {
+    return (
+      <TableRow key={row[0]}>
+        <TableCell>{row[0]}</TableCell>
+        <TableCell>{row[1]}</TableCell>
+        <TableCell>{row[2]}</TableCell>
+      </TableRow>
+    );
+  });
   return (
     <div>
       <h3>Organelles</h3>
@@ -51,29 +121,20 @@ export default function Organelles() {
       <h3>Contact Sites</h3>
       <Table>
         <TableHead>
-            <TableCell style={{ width: "20em" }}>Organelle</TableCell>
-            <TableCell>File Name</TableCell>
+          <TableCell style={{ width: "20em" }}>Organelle</TableCell>
+          <TableCell>File Name</TableCell>
         </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>Organelle</TableCell>
-            <TableCell>file_name</TableCell>
-          </TableRow>
-        </TableBody>
+        <TableBody>{contactRows}</TableBody>
       </Table>
       <h3>Analysis</h3>
       <Table>
         <TableHead>
-            <TableCell style={{ width: "20em" }}>Organelle</TableCell>
-            <TableCell>File Name</TableCell>
-            <TableCell>Description</TableCell>
+          <TableCell style={{ width: "20em" }}>Organelle</TableCell>
+          <TableCell>File Name</TableCell>
+          <TableCell>Description</TableCell>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>Organelle</TableCell>
-            <TableCell>file_name</TableCell>
-            <TableCell>description</TableCell>
-          </TableRow>
+          {analysisRows}
         </TableBody>
       </Table>
     </div>
