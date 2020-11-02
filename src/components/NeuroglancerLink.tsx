7f1eece02eaf55a68fabbf8fd20a3819859a0624
Jody Clements <jody@neomorphic.co.uk>
2020-11-02 15:45:34 -0500

    Reformats NeuroglancerLink code with prettier

--- src/components/NeuroglancerLink.tsx
+++ src/components/NeuroglancerLink.tsx
@@ -1,14 +1,16 @@
 import { Box, createStyles, Link, Button } from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import React, {useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import React, { useContext } from "react";
 import { Dataset, DatasetView, Volume } from "../api/datasets";
 import { AppContext } from "../context/AppContext";
 import LaunchIcon from "@material-ui/icons/Launch";
 import WarningIcon from "@material-ui/icons/Warning";
 
 const useStyles = makeStyles(theme => ({
-  root: {flexGrow: 1,
-        fontFamily: "'Proxima Nova W01',Arial,Helvetica,sans-serif"},
+  root: {
+    flexGrow: 1,
+    fontFamily: "'Proxima Nova W01',Arial,Helvetica,sans-serif"
+  },
 
   link: {
     color: theme.palette.primary.contrastText,
@@ -18,41 +20,47 @@ const useStyles = makeStyles(theme => ({
 }));
 
 type NeuroglancerLinkProps = {
-    dataset: Dataset;
-    view: DatasetView
-    checkState: Map<string, boolean>;
-  };
+  dataset: Dataset;
+  view: DatasetView;
+  checkState: Map<string, boolean>;
+};
 
+export default function NeuroglancerLink({
+  dataset,
+  view,
+  checkState
+}: NeuroglancerLinkProps) {
+  const classes = useStyles();
+  const [appState] = useContext(AppContext);
+  const neuroglancerAddress = appState.neuroglancerAddress;
+  const webGL2Enabled = appState.webGL2Enabled;
 
-export default function NeuroglancerLink({dataset, view, checkState}: NeuroglancerLinkProps){
-    const classes = useStyles();
-    const [appState,] = useContext(AppContext);
-    const neuroglancerAddress = appState.neuroglancerAddress;
-    const webGL2Enabled = appState.webGL2Enabled;
-    
-    const local_view = {...view};
-    local_view.volumeKeys = [];
-    dataset.volumes.forEach((value: Volume, key: string) => {
-      if (checkState.get(key)) { local_view.volumeKeys.push(key) }
-    });
-    
-  
-    if (local_view.volumeKeys.length === 0) { return <div> No layers selected </div> }
-    else {
-      return (
-        <Box>
-          <Button
-            className={classes.link}
-            href={`${neuroglancerAddress
-              }${dataset.makeNeuroglancerViewerState(local_view)}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            endIcon={<LaunchIcon fontSize="small" />}
-          >
-            View
-        </Button>
-          {!webGL2Enabled && <WarningIcon />}
-        </Box>
-      );
+  const local_view = { ...view };
+  local_view.volumeKeys = [];
+  dataset.volumes.forEach((value: Volume, key: string) => {
+    if (checkState.get(key)) {
+      local_view.volumeKeys.push(key);
     }
-  };
+  });
+
+  if (local_view.volumeKeys.length === 0) {
+    return <div> No layers selected </div>;
+  } else {
+    return (
+      <Box>
+        <Button
+          className={classes.link}
+          href={`${neuroglancerAddress}${dataset.makeNeuroglancerViewerState(
+            local_view
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          endIcon={<LaunchIcon fontSize="small" />}
+        >
+          View
+        </Button>
+        {!webGL2Enabled && <WarningIcon />}
+      </Box>
+    );
+  }
+}
