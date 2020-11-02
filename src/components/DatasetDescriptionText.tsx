3f97aa34ed013c7e8c8115ed1a96a27fd942d291
Jody Clements <jody@neomorphic.co.uk>
2020-11-02 15:56:44 -0500

    Converts DatasetDescriptionText to 2 column layout

--- src/components/DatasetDescriptionText.tsx
+++ src/components/DatasetDescriptionText.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
-import {DatasetDescription} from "../api/dataset_description";
-import ReactHtmlParser from 'react-html-parser';
+import { DatasetDescription } from "../api/dataset_description";
+import ReactHtmlParser from "react-html-parser";
 
 type DescriptionTextProps = {
   titleLink: string;
@@ -13,11 +14,10 @@ type DescriptionTextProps = {
 const useStyles: any = makeStyles((theme: Theme) =>
   createStyles({
     title: {
-      color: theme.palette.primary.main,
+      color: theme.palette.primary.main
     }
   })
 );
-
 
 export function DatasetDescriptionPreview(props: DescriptionTextProps) {
   const description = props.datasetDescription;
@@ -25,61 +25,68 @@ export function DatasetDescriptionPreview(props: DescriptionTextProps) {
   if (description === undefined) {
     return (
       <Box>
-<Typography variant="h6" className={classes.title}>
-  {ReactHtmlParser('No description provided')}
-</Typography>
-</Box>
-    );  
-  }
-  else 
-  return (
-    <Box>
-      <Typography variant="h6" className={classes.title}>
-        {ReactHtmlParser(description.Title)}
-      </Typography>
-      {[...Object.keys(description.Summary)].map(value => (
-        <p key={value}>
-          <strong>{ReactHtmlParser(value)}</strong>:{" "}
-          {ReactHtmlParser(description.Summary[value])}
-        </p>
-      ))}
-    </Box>
-  );
+        <Typography variant="h6" className={classes.title}>
+          {ReactHtmlParser("No description provided")}
+        </Typography>
+      </Box>
+    );
+  } else
+    return (
+      <Box>
+        <Typography variant="h6" className={classes.title}>
+          {ReactHtmlParser(description.Title)}
+        </Typography>
+        {[...Object.keys(description.Summary)].map(value => (
+          <p key={value}>
+            <strong>{ReactHtmlParser(value)}</strong>:{" "}
+            {ReactHtmlParser(description.Summary[value])}
+          </p>
+        ))}
+      </Box>
+    );
 }
 
 export function DatasetDescriptionFull(props: DescriptionTextProps) {
   const description = props.datasetDescription;
   const classes = useStyles();
 
-  if (description === undefined)
-  {
-    return <Box>
-  <Typography variant="h6" className={classes.title}>
-    {ReactHtmlParser('No description provided')}
-  </Typography>
-  </Box>
+  if (description === undefined) {
+    return (
+      <Box>
+        <Typography variant="h6" className={classes.title}>
+          {ReactHtmlParser("No description provided")}
+        </Typography>
+      </Box>
+    );
+  } else {
+    return (
+      <>
+        <Typography variant="h6" className={classes.title}>
+          {ReactHtmlParser(description.Title)}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            {[...Object.keys(description["About this sample"])].map(value => (
+              <p key={value}>
+                <strong>{ReactHtmlParser(value)}</strong>:{" "}
+                {ReactHtmlParser(description["About this sample"][value])}
+              </p>
+            ))}
+          </Grid>
+          <Grid item xs={6}>
+            {[...Object.keys(description["Acquisition information"])].map(
+              value => (
+                <p key={value}>
+                  <strong>{ReactHtmlParser(value)}</strong>:{" "}
+                  {ReactHtmlParser(
+                    description["Acquisition information"][value]
+                  )}
+                </p>
+              )
+            )}
+          </Grid>
+        </Grid>
+      </>
+    );
   }
-else {
-  return (
-    <Box>
-      <Typography variant="h6" className={classes.title}>
-        {ReactHtmlParser(description.Title)}
-      </Typography>
-
-    {[...Object.keys(description["About this sample"])].map(value => (
-        <p key={value}>
-          <strong>{ReactHtmlParser(value)}</strong>:{" "}{ReactHtmlParser(description["About this sample"][value])}
-        </p>
-      ))}
-
-
-{[...Object.keys(description["Acquisition information"])].map(value => (
-        <p key={value}>
-          <strong>{ReactHtmlParser(value)}</strong>:{" "}{ReactHtmlParser(description["Acquisition information"][value])}
-        </p>
-      ))}
-    </Box>
-  );
-}
-  
 }
