b51285c34d4f2cb6a984af2823c6257036d2d6b7
Jody Clements <jody@neomorphic.co.uk>
2021-03-22 13:20:14 -0400

    Updates cypherBuilder neo4j output.
    This improves the output of the neo4j cypher, by makeing more complete
    queries and removing some of the broken ones.

--- src/utils/cypherBuilder.ts
+++ src/utils/cypherBuilder.ts
@@ -2,31 +2,43 @@
 // value and return a cypher query to run.
 
 interface builderProps {
-  dataset: string,
-  organelles: string[],
-  measurements: string[]
-};
+  dataset: string;
+  organelles: string[];
+  measurements: string[];
+}
 
-export default function cypherBuilder ({ dataset, organelles, measurements }: builderProps): string | undefined {
-  const query = undefined;
+export default function cypherBuilder({
+  dataset,
+  organelles,
+  measurements
+}: builderProps): string | undefined {
+  let query = undefined;
   // are all parameters present
-  if (!dataset || !organelles || organelles.length < 1 || !measurements || measurements.length < 1) {
-    return query;
+  if (
+    dataset &&
+    organelles &&
+    organelles.length > 0 &&
+    measurements &&
+    measurements.length > 0
+  ) {
+    // is this a contact query or a single organelle query
+    const isContactQuery = organelles.length > 1;
+    if (isContactQuery) {
+      // organelle order for the contact tag is important as there are only certain combinations
+      // eg: er_golgi, mito_mt, mt_nucleus
+      const contactMatch = `MATCH p=(organelleA)-[contact:\`${dataset}|${organelles[0]}_${organelles[1]}_contacts\`]->(organelleB)`;
+      const contactParameters = measurements.map((m: string) => `contact.${m}`);
+      contactParameters.push("organelleA.ID", "organelleB.ID");
+      const contactReturn = `RETURN ${contactParameters.join(", ")}`;
+      const completeCypher = `${contactMatch} ${contactReturn} LIMIT 30;`;
+      console.log(completeCypher);
+    } else {
+      const returnParameters = measurements
+        .map((m: string) => `organelle.${m}`)
+        .join(", ");
+
+      query = `MATCH(organelle:\`${dataset}|${organelles[0]}\`) RETURN ${returnParameters} LIMIT 3000;`;
+    }
   }
-
-  // is this a contact query or a single organelle query
-  const isContactQuery = organelles.length > 1;
-  if (isContactQuery) {
-    // organelle order for the contact tag is important as there are only certain combinations
-    // eg: er_golgi, mito_mt, mt_nucleus
-    const contactMatch = `MATCH p=(organelleA)-[contact:\`${dataset}|${organelles[0]}_${organelles[1]}_contacts\`]->(organelleB)`;
-    const contactParameters = measurements.map((m: string) => `contact.${m}`);
-    contactParameters.push('organelleA.ID', 'organelleB.ID')
-    const contactReturn = `RETURN ${contactParameters.join(', ')}`;
-    return `${contactMatch} ${contactReturn}`;
-  }
-
-  const returnParameters = measurements.map((m: string) => `organelle.${m}`).join(', ')
-
-  return `MATCH(organelle:\`${dataset}|${organelles[0]}\`) RETURN ${returnParameters} LIMIT 30`;
+  return query;
 }
