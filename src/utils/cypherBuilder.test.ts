adac31a286d903829d43ba7a07a86fab58a0757e
Jody Clements <jody@neomorphic.co.uk>
2021-03-23 09:28:42 -0400

    Improves cypherBuilder output.
    Now it will only generate cypher queries for valid contact pairs.
    All other combinations will throw an error.

--- src/utils/cypherBuilder.test.ts
+++ src/utils/cypherBuilder.test.ts
@@ -7,7 +7,7 @@ test("creates cypher for er volume", () => {
       organelles: ["er"],
       measurements: ["volume"]
     })
-  ).toBe("MATCH(organelle:`jrc_hela-2|er`) RETURN organelle.volume");
+  ).toBe("MATCH(organelle:`jrc_hela-2|er`) RETURN organelle.volume;");
 });
 
 test("creates cypher for er contact with golgi", () => {
@@ -17,5 +17,35 @@ test("creates cypher for er contact with golgi", () => {
       organelles: ["er","golgi"],
       measurements: ["planarity"]
     })
-  ).toBe("MATCH p=(organelleA)-[contact:`jrc_hela-2|er_golgi_contacts`]->(organelleB) RETURN contact.planarity, organelleA.ID, organelleB.ID");
+  ).toBe("MATCH p=(organelleA:`jrc_hela-2|er`)-[contact:`jrc_hela-2|er_golgi_contacts`]->(organelleB:`jrc_hela-2|golgi`) RETURN contact.planarity, organelleA.ID, organelleB.ID LIMIT 30;");
+});
+
+test("creates cypher for golgi contact with er", () => {
+  expect(
+    cypherBuilder({
+      dataset: "jrc_hela-2",
+      organelles: ["golgi", "er"],
+      measurements: ["planarity"]
+    })
+  ).toBe("MATCH p=(organelleA:`jrc_hela-2|er`)-[contact:`jrc_hela-2|er_golgi_contacts`]->(organelleB:`jrc_hela-2|golgi`) RETURN contact.planarity, organelleA.ID, organelleB.ID LIMIT 30;");
+});
+
+test("creates cypher for er contact with er", () => {
+  expect(() =>
+    cypherBuilder({
+      dataset: "jrc_hela-2",
+      organelles: ["er", "er"],
+      measurements: ["planarity"]
+    })
+  ).toThrow("There are no contacts between er and er");
+});
+
+test("creates cypher for nucleus contact with er", () => {
+  expect(() =>
+    cypherBuilder({
+      dataset: "jrc_hela-2",
+      organelles: ["nucleus", "er"],
+      measurements: ["length"]
+    })
+  ).toThrow("There are no contacts between nucleus and er");
 });
--- src/utils/cypherBuilder.ts
+++ src/utils/cypherBuilder.ts
@@ -7,6 +7,24 @@ interface builderProps {
   measurements: string[];
 }
 
+const contactTypes = [
+  "endo_er",
+  "endo_golgi",
+  "endo_mt",
+  "er_golgi",
+  "er_mito",
+  "er_mt",
+  "er_pm",
+  "er_ribo",
+  "er_vesicle",
+  "er-periph_mito",
+  "er-periph_mt",
+  "er-periph_ribo",
+  "mt_nucleus",
+  "mt_pm",
+  "mt_vesicle"
+];
+
 export default function cypherBuilder({
   dataset,
   organelles,
@@ -26,18 +44,38 @@ export default function cypherBuilder({
     if (isContactQuery) {
       // organelle order for the contact tag is important as there are only certain combinations
       // eg: er_golgi, mito_mt, mt_nucleus
-      const contactMatch = `MATCH p=(organelleA)-[contact:\`${dataset}|${organelles[0]}_${organelles[1]}_contacts\`]->(organelleB)`;
+
+      // check organelles are in the allowed contacts
+      const contactType = contactTypes.filter(
+        type =>
+          type === organelles.join("_") ||
+          type ===
+            organelles
+              .slice() // prevent in place modification of the organelles array
+              .reverse()
+              .join("_")
+      )[0];
+
+      if (!contactType) {
+        throw Error(
+          `There are no contacts between ${organelles[0]} and ${organelles[1]}`
+        );
+      }
+
+      const selected = contactType.split("_");
+
+      const contactMatch = `MATCH p=(organelleA:\`${dataset}|${selected[0]}\`)-[contact:\`${dataset}|${contactType}_contacts\`]->(organelleB:\`${dataset}|${selected[1]}\`)`;
       const contactParameters = measurements.map((m: string) => `contact.${m}`);
       contactParameters.push("organelleA.ID", "organelleB.ID");
       const contactReturn = `RETURN ${contactParameters.join(", ")}`;
       const completeCypher = `${contactMatch} ${contactReturn} LIMIT 30;`;
-      console.log(completeCypher);
+      query = completeCypher;
     } else {
       const returnParameters = measurements
         .map((m: string) => `organelle.${m}`)
         .join(", ");
 
-      query = `MATCH(organelle:\`${dataset}|${organelles[0]}\`) RETURN ${returnParameters} LIMIT 3000;`;
+      query = `MATCH(organelle:\`${dataset}|${organelles[0]}\`) RETURN ${returnParameters};`;
     }
   }
   return query;
