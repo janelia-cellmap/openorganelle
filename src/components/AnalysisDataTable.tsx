104035ecf2c70deac03d2f4d70e5a949e8da9afc
Jody Clements <jody@neomorphic.co.uk>
2021-03-26 09:11:28 -0400

    Adds better UI elements for pagination.
    Styles match the site.

--- src/components/AnalysisDataTable.tsx
+++ src/components/AnalysisDataTable.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { useTable, useSortBy, usePagination } from "react-table";
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    select: {
+      border: `1px solid ${theme.palette.primary.main}80`,
+      color: theme.palette.primary.main,
+      padding: "0.3em",
+      borderRadius: "5px"
+    }
+  })
+);
 
 interface AnalysisDataTableProps {
-  data: any[],
-  columns: any[]
-};
+  data: any[];
+  columns: any[];
+}
 
-export default function AnalysisDataTable({ data, columns }: AnalysisDataTableProps) {
+export default function AnalysisDataTable({
+  data,
+  columns
+}: AnalysisDataTableProps) {
   const {
     getTableProps,
     getTableBodyProps,
@@ -30,22 +47,29 @@ export default function AnalysisDataTable({ data, columns }: AnalysisDataTablePr
   } = useTable(
     {
       columns,
-      data
+      data,
+      initialState: {
+        pageSize: 20
+      }
     },
     useSortBy,
     usePagination
   );
 
+  const classes = useStyles();
+
   return (
     <>
-      <Table  size="small" {...getTableProps()}>
+      <Table size="small" {...getTableProps()}>
         <TableHead>
           {headerGroups.map(headerGroup => (
             <TableRow {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
                 // Add the sorting props to control sorting. For this example
                 // we can add them into the header props
-                <TableCell {...column.getHeaderProps(column.getSortByToggleProps())}>
+                <TableCell
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                >
                   {column.render("Header")}
                   {/* Add a sort direction indicator */}
                   <span>
@@ -67,7 +91,9 @@ export default function AnalysisDataTable({ data, columns }: AnalysisDataTablePr
               <TableRow {...row.getRowProps()}>
                 {row.cells.map(cell => {
                   return (
-                    <TableCell {...cell.getCellProps()}>{cell.render("Cell")}</TableCell>
+                    <TableCell {...cell.getCellProps()}>
+                      {cell.render("Cell")}
+                    </TableCell>
                   );
                 })}
               </TableRow>
@@ -77,27 +103,50 @@ export default function AnalysisDataTable({ data, columns }: AnalysisDataTablePr
       </Table>
       <br />
       <div className="pagination">
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+        <Button
+          size="small"
+          color="primary"
+          variant="outlined"
+          onClick={() => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
           {"<<"}
-        </button>{" "}
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+        </Button>{" "}
+        <Button
+          size="small"
+          color="primary"
+          variant="outlined"
+          onClick={() => previousPage()}
+          disabled={!canPreviousPage}
+        >
           {"<"}
-        </button>{" "}
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
+        </Button>{" "}
+        <Button
+          size="small"
+          color="primary"
+          variant="outlined"
+          onClick={() => nextPage()}
+          disabled={!canNextPage}
+        >
           {">"}
-        </button>{" "}
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+        </Button>{" "}
+        <Button
+          size="small"
+          color="primary"
+          variant="outlined"
+          onClick={() => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
           {">>"}
-        </button>{" "}
-        <span>
+        </Button>{" "}
+        <Typography component="span">
           Page{" "}
           <strong>
             {pageIndex + 1} of {pageOptions.length}
           </strong>{" "}
-        </span>
-        <span>
           | Go to page:{" "}
           <input
+            className={classes.select}
             type="number"
             defaultValue={pageIndex + 1}
             onChange={e => {
@@ -106,8 +155,9 @@ export default function AnalysisDataTable({ data, columns }: AnalysisDataTablePr
             }}
             style={{ width: "100px" }}
           />
-        </span>{" "}
+        </Typography>{" "}
         <select
+          className={classes.select}
           value={pageSize}
           onChange={e => {
             setPageSize(Number(e.target.value));
