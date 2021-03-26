cc4a57d651f87bb3f632c44e5b22e95718c4419a
Jody Clements <jody@neomorphic.co.uk>
2021-03-26 16:02:12 -0400

    Adds cypher output above results.
    This is useful for debugging, but might also help users if they want to
    start writing queries of their own.

--- src/components/Analysis.tsx
+++ src/components/Analysis.tsx
@@ -220,6 +220,7 @@ export default function Analysis() {
           {measurementItems}
         </Select>
       </FormControl>
+      <code>{cypher}</code>
       {cypher ? (
         <AnalysisResults
           cypher={cypher}
