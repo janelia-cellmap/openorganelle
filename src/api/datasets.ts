5b5ad9121f16871426b37a2fc137eb41d7cd8c9a
Yurii Zubov <zubov452@gmail.com>
2024-12-16 10:16:45 -0500

    Add 'Annotation Layers' section to layer selection panel for a dataset webpage

--- src/api/datasets.ts
+++ src/api/datasets.ts
@@ -16,6 +16,7 @@ export const contentTypeDescriptions = new Map<ContentType, ContentTypeMetadata>
 
 contentTypeDescriptions.set('em', { label: "EM Layers", description: "EM data." });
 contentTypeDescriptions.set('lm', { label: "LM Layers", description: "Light microscopy data." });
+contentTypeDescriptions.set('annotation', { label: "Annotation Layers", description: "Manual Annotations of regions of interest within volumetric EM/LM data." });
 contentTypeDescriptions.set('segmentation', { label: "Segmentation Layers", description: "Predictions that have undergone refinements such as thresholding, smoothing, size filtering, and connected component analysis. Double Left Click a segmentation to turn on/off a 3D rendering." });
 contentTypeDescriptions.set('prediction', { label: "Prediction Layers", description: "Raw distance transform inferences scaled from 0 to 255. A voxel value of 127 represent a predicted distance of 0 nm." });
 contentTypeDescriptions.set('analysis', { label: "Analysis Layers", description: "Results of applying various analysis routines on raw data, predictions, or segmentations." });
