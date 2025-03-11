28816d2a2bef0af43e2a3377afdc1ba2009ee350
Yurii Zubov <zubov452@gmail.com>
2025-03-11 12:26:18 -0400

    Fix typo: Hz -> MHz

--- src/components/DatasetDescriptionText.tsx
+++ src/components/DatasetDescriptionText.tsx
@@ -132,7 +132,7 @@ export function DatasetAcquisition({
         </Typography>
         <p><strong>Imaging duration (days)</strong> : {params.durationDays ? params.durationDays : "Unknown"}</p>
         <p><strong>Bias (Volts)</strong>: {params.biasV ? params.biasV : "Unknown"}</p>
-        <p><strong>Scan rate (Hz)</strong>: {params.scanHz ? params.scanHz : "Unknown"}</p>
+        <p><strong>Scan rate (MHz)</strong>: {params.scanHz ? params.scanHz : "Unknown"}</p>
         <p><strong>Current (nA)</strong>: {params.currentNA ? params.currentNA : "Unknown"}</p>
         <p><strong>Primary energy (eV)</strong>: {params.landingEnergyEV ? params.landingEnergyEV : "Unknown"}</p>
       </>
